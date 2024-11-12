4f3db46452679f8888126084b778bb5f1172da7b
JonathasLopes <jon.athas@hotmail.com>
2024-11-11 23:56:12 -0300

    email changed

--- src/Configurations/emailSenderSS.ts
+++ src/Configurations/emailSenderSS.ts
@@ -2,8 +2,13 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
   service: process.env.SSMAILER,
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
+  logger: true,
+  debug: true,
+  tls: {
+    ciphers: "SSLv3"
+  },
   auth: {
     user: process.env.SSEMAIL,
     pass: process.env.SSPASSWORD
