ba48303640884c6fe9e1c5b68f583c1c8810dbf4
JonathasLopes <jon.athas@hotmail.com>
2025-03-15 23:29:36 -0300

    wedding added

---
+++ src/Services/WeddingService.ts
@@ -0,0 +1,45 @@
+import { Request, Response } from 'express';
+import handlebars from 'handlebars';
+import fs from 'fs';
+import path from 'path';
+import sgMail from '@sendgrid/mail';
+
+class WeddingService {
+    async sendNotConfirmed(request: Request, response: Response) {
+        const { names } = request.body;
+
+        if (!names) {
+            response.status(400).json({ message: 'Data missing!' });
+        }
+
+        const templatePath = path.join(__dirname, '../Templates/Wedding', 'NotConfirmed.hbs');
+        const templateSource = fs.readFileSync(templatePath, 'utf-8');
+        const compiledTemplate = handlebars.compile(templateSource);
+
+        const context = {
+            names: names.split(","),
+            year: new Date().getFullYear()
+        };
+
+        const htmlToSend = compiledTemplate(context);
+
+        const mailOptions = {
+            from: process.env.SSEMAIL,
+            to: process.env.MYEMAIL,
+            subject: 'Wedding - Jonathas e Bianca',
+            html: htmlToSend
+        };
+
+        try {
+            await sgMail.send(mailOptions);
+
+            response.json({ message: "Email sent!" });
+        } catch (error) {
+            response.status(500).json({ message: `There was a problem sending email! ${error}` });
+        }
+
+        return;
+    }
+}
+
+export default WeddingService
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import SecretSurpriseService from './Services/secretSurpriseService';
+import WeddingService from './Services/WeddingService';
 
 const router = Router();
 
 const secretSurpriseService = new SecretSurpriseService();
+const weddingService = new WeddingService();
 
 router.get("/", (req, res) => {res.send("Estou on!")});
 router.post("/secretSurprise/mfa", secretSurpriseService.sendMFA);
+
+router.post("/wedding/notConfirmed", weddingService.sendNotConfirmed);
 
 export {router};
